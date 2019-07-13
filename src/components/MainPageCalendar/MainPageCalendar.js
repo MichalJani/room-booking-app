41c9dd623214fc0664fe0446f3cb336ed3a666fb
Michal Janicki <michal.janicki1@gmail.com>
2019-07-13 20:27:02 +0100

    Add MainPage components

---
+++ src/components/MainPageCalendar/MainPageCalendar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from '@material-ui/core';
+
+class MainPageCalendar extends Component {
+  render() {
+    return (
+      <Container>
+
+      </Container>
+    );
+  }
+}
+
+export default MainPageCalendar;
