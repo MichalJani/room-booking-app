923c0ea382f3187097a5b6df148b07b11eda79a1
Jakub Karwatka <jakubkarwatka@gmail.com>
2019-07-14 20:50:40 +0200

    add styling for StatusSection, fix bugs

--- src/components/StatusSection/StatusSection.js
+++ src/components/StatusSection/StatusSection.js
@@ -1,15 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Typography, Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-
+const useStyles = makeStyles(theme => ({
+  container: {
+    backgroundColor: "grey"
+  }
+}));
 const StatusSection = ({ status, roomName }) => {
+  const classes = useStyles();
   return (
-    <Container maxWidth="lg">
-      <Typography variant="h3" component="h3">
+    <Container maxWidth="lg" className={classes.container}>
+      <Typography variant="h4" component="h4">
         {status}
       </Typography>
-      <Typography variant="h2" component="h2">
+      <Typography variant="h3" component="h3">
         {roomName}
       </Typography>
     </Container>
