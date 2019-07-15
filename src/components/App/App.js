8b730a79b9857b94ef1b626c284463b5b111bed2
Michal Janicki <michal.janicki1@gmail.com>
2019-07-15 21:18:10 +0100

    Correct redux-thunk package and make App arrow function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../../store';
 
-function App() {
+const App = () => {
   return (
     <Provider store={store}>
       <div className="App">
