eb923603a3fa47946eb7e247c3fad56dab3d0d43
Michal Janicki <michal.janicki1@gmail.com>
2019-07-17 01:11:43 +0200

    Add grid to the mainPage component

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { Container } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+import MainPageStatus from '../MainPageStatus/MainPageStatus';
+import MainPageTimeline from '../MainPageTimeline/MainPageTimeline';
 
 class MainPage extends Component {
   render() {
     return (
       <Container>
-
+        <Grid container spacing={0}>
+          <Grid item md={8}>
+            <MainPageStatus />
+          </Grid>
+          <Grid item md={4}>
+            <MainPageTimeline />
+          </Grid>
+        </Grid>
       </Container>
     );
   }
---
+++ src/components/MainPageStatus/MainPageStatus.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+class MainPageStatus extends Component {
+  render() {
+    return (
+      <Container>
+
+      </Container>
+    );
+  }
+}
+
+export default MainPageStatus;
