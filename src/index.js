8d72f8e2968abb627092004f23d6dbce66585be3
Michal Janicki <michal.janicki1@gmail.com>
2019-07-17 00:32:52 +0200

    Add theme.js with customized mui theme

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { CssBaseline } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
 import "./index.css";
 import App from "./components/App/App";
 
 ReactDOM.render(
-  <>
+  <ThemeProvider theme={theme}>
     <CssBaseline />
     <App />
-  </>,
+  </ThemeProvider>,
   document.getElementById("root")
 );
