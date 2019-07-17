b971facb766591ef0d75f044c36a3853821d3123
Michal Janicki <michal.janicki1@gmail.com>
2019-07-17 18:52:59 +0200

    Remove unnecessary files

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 
-import MainPageStatus from '../MainPageStatus/MainPageStatus';
-import MainPageTimeline from '../MainPageTimeline/MainPageTimeline';
 
 class MainPage extends Component {
   render() {
@@ -11,10 +9,10 @@ class MainPage extends Component {
       <Container>
         <Grid container spacing={0}>
           <Grid item md={8}>
-            <MainPageStatus />
+
           </Grid>
           <Grid item md={4}>
-            <MainPageTimeline />
+
           </Grid>
         </Grid>
       </Container>
