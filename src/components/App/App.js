c43d828fa43e8d5e6df991d87d1841c9c6919aa3
Michal Janicki <michal.janicki1@gmail.com>
2019-07-18 11:17:37 +0100

    Remove package-lock.json

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-
-      import { Container } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import { Provider } from 'react-redux';
 import store from '../../store';
 
 const App = () => {
   return (
     <Provider store={store}>
-       <Container>
-    <MainPage />
-  </Container>
+      <Container>
+        <MainPage />
+      </Container>
     </Provider>
   );
 }
