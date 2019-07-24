cd9014784ae9c98c8ba48111fe067001fa2b4b91
Jakub Karwatka <jakubkarwatka@gmail.com>
2019-07-24 19:07:04 +0200

    add CalendarSideBar implementation

---
+++ src/components/CalendarSideBar/CalendarSideBar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import CalendarCard from '../CalendarCard/CalendarCard';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+});
+
+const CalendarSideBar = () => {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    isOpen: false,
+  });
+
+  const toggleDrawer = () => event => {
+    const openState = !state.isOpen;
+    setState({ ...state, isOpen: openState });
+  };
+
+  const sideList = () => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer()}
+      onKeyDown={toggleDrawer()}
+    >
+      <CalendarCard />
+
+      <CalendarCard />
+
+      <CalendarCard />
+
+      <CalendarCard />
+    </div>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer()}>Open Calendar</Button>
+      <SwipeableDrawer
+        anchor="right"
+        open={state.isOpen}
+        onClose={toggleDrawer()}
+        onOpen={toggleDrawer()}
+      >
+        {sideList()}
+      </SwipeableDrawer>
+    </div>
+  );
+};
+
+export default CalendarSideBar;
