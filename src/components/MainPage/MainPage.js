65fe16366e3fe2ebd5ebb9f54d65cf08d1121d1d
Michal Janicki <michal.janicki1@gmail.com>
2019-07-24 18:20:13 +0200

    Clarify reducer

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import { Button } from '@material-ui/core';
-import { connect } from 'react-redux';
-import { getAllEvents } from '../../redux/actions/actionCreators';
-
-const MainPage = ({ getAllEvents }) => {
-
-  return (
-    <Container>
-      <Grid container spacing={0}>
-        <Grid item md={8}>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            onClick={getAllEvents}
-          >
-            Klikej synek!
-            </Button>
-        </Grid>
-        <Grid item md={4}>
-        </Grid>
-      </Grid>
-    </Container>
-  );
-
-}
-=======
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
@@ -86,8 +55,9 @@ const MainPage = () => {
     </Container>
   );
 };
->>>>>>> develop
 
+// const mapStateToProps = state => ({
 
+// })
 
 export default connect(null, { getAllEvents })(MainPage);
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,5 @@ import { combineReducers } from 'redux';
 import eventReducer from './eventReducer';
 
 export default combineReducers({
-<<<<<<< HEAD
-  events: (state = [], action) => { console.log(action); return state }
-=======
   events: eventReducer
->>>>>>> develop
 });
