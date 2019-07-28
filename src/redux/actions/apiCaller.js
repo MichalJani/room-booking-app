86b54c7aa0dcf10a9cf9aa37755f10ebe8b2d3b2
Michal Janicki <michal.janicki1@gmail.com>
2019-07-28 15:38:46 +0100

    Remove comments from the file

--- src/redux/actions/apiCaller.js
+++ src/redux/actions/apiCaller.js
@@ -1,8 +1,4 @@
-// import axios from 'axios';
 
-// // export const BASE_URL = 'https://www.googleapis.com/calendar/v3/calendars';
-// // https://www.googleapis.com/calendar/v3/calendars/calendarId/events
-// // ?key=[YOUR_API_KEY]
 // import { gapi } from '../../gapi';
 // // export const key = '?key=';
 // // export
@@ -21,24 +17,4 @@
 //       return error;
 //     });
 
-// }
-// export default function callApi(calendarId, method = 'GET', body) {
-//   return axios({
-//     method,
-//     url: `${BASE_URL}/${calendarId}/events`,
-//     data: JSON.stringify(body),
-//     headers: {
-//       'Authorization': 'Bearer [YOUR_ACCESS_TOKEN]',
-
-//       'Accept': 'application/json',
-
-//       'Content-Type': 'application/json'
-//     }
-//   })
-//     .then(res => {
-//       return res.data;
-//     })
-//     .catch(error => {
-//       return error;
-//     });
 // }
