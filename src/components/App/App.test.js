1fc717d609125bbf003faad52ce44e85b38c51f5
Michal Janicki <michal.janicki1@gmail.com>
2019-07-31 18:05:01 +0200

    Add config.js to gitignore

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,37 +1,11 @@
 import React from 'react';
-// import { shallow, mount, render } from 'enzyme';
-import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
-
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<App />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-// test('Renders without crashing', () => {
-//   const component = renderer.create(
-//     <Link page="http://www.facebook.com">Facebook</Link>,
-//   );
-//   let tree = component.toJSON();
-//   expect(tree).toMatchSnapshot();
-
-//   // manually trigger the callback
-//   tree.props.onMouseEnter();
-//   // re-rendering
-//   tree = component.toJSON();
-//   expect(tree).toMatchSnapshot();
-
-//   // manually trigger the callback
-//   tree.props.onMouseLeave();
-//   // re-rendering
-//   tree = component.toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
+describe('App', () => {
+  // it('Should render without crashing',
+  //   () => {
+  //     const wrapper = shallow(<App />);
+  //     expect(wrapper).toBeTruthy();
+  //   })
+})
