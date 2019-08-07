a272d71995d6888b50969349b6e745dd5181c75d
Michal Janicki <michal.janicki1@gmail.com>
2019-08-07 15:48:24 +0100

    Remove buged tests

--- src/components/CalendarSideBar/CalendarSideBar.test.js
+++ src/components/CalendarSideBar/CalendarSideBar.test.js
@@ -3,18 +3,18 @@ import { shallow, mount } from 'enzyme';
 import CalendarSideBar from './CalendarSideBar';
 
 
-fdescribe('CalendarSideBar', () => {
+describe('CalendarSideBar', () => {
   it('Should render without crashing',
     () => {
       const wrapper = shallow(<CalendarSideBar />);
       expect(wrapper).toBeTruthy();
     });
 
-  fit('Should set Drawer state to true on click', () => {
-    const wrapper = mount(<CalendarSideBar />);
-    const variable = wrapper.find('[data-test="CalSideBarButton"]')
-    console.log(variable)
-    expect(wrapper.state('isOpen')).toEqual(true);
-    // wrapper.unmount();
-  });
+  // fit('Should set Drawer state to true on click', () => {
+  //   const wrapper = mount(<CalendarSideBar />);
+  //   const variable = wrapper.find('[data-test="CalSideBarButton"]')
+  //   console.log(variable)
+  //   expect(wrapper.state('isOpen')).toEqual(true);
+  //   // wrapper.unmount();
+  // });
 });
--- src/components/QuickReservationButtonGroup/QuickReservationButtonGroup.test.js
+++ src/components/QuickReservationButtonGroup/QuickReservationButtonGroup.test.js
@@ -10,17 +10,24 @@ describe('QuickReservationButtonGroup', () => {
       expect(wrapper).toBeTruthy();
     });
 
-
-  //   it('should call mockFunction on button click', () => {
-  //     const mockFunction = jest.fn();
-  //     const wrapper = shallow(
-  //       <QuickReservationButtonGroup />
-  //     );
-  //     // [href="foo"]
-  //     wrapper.find(Button[key = 15]).simulate('click');
-  //     expect(mockFunction).toHaveBeenCalled();
-  //     wrapper.unmount();
+  // it('Should render ButtonGroup without crashing',
+  //   () => {
+  //     const wrapper = shallow(<QuickReservationButtonGroup buttonVariants={[]} addEventRequest={jest.fn()} />);
+  //     const ButtonGroup = wrapper.find(<ButtonGroup />)
+  //     expect(ButtonGroup).toBeTruthy();
   //   });
+
+
+  // it('should call mockFunction on button click', () => {
+  //   const mockFunction = jest.fn();
+  //   const wrapper = shallow(
+  //     <QuickReservationButtonGroup />
+  //   );
+  //   // [href="foo"]
+  //   wrapper.find(Button[key = 15]).simulate('click');
+  //   expect(mockFunction).toHaveBeenCalled();
+  //   wrapper.unmount();
+  // });
 
 });
 
