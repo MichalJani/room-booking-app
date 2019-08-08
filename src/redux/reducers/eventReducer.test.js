e9dc3c84111f7e77e4e97a5cfeb0e0ecc0906002
Michal Janicki <michal.janicki1@gmail.com>
2019-08-08 13:39:26 +0100

    Add eventReducer test

---
+++ src/redux/reducers/eventReducer.test.js
@@ -0,0 +1,31 @@
+import {
+  GET_ALL_EVENTS_ERROR,
+  GET_ALL_EVENTS_START,
+  GET_ALL_EVENTS_FINISH
+} from '../actions/actionTypes'
+import eventReducer from './eventReducer'
+import { eventStates } from '../../utils/consts'
+
+describe('Event Reducer', () => {
+  it('Should return default state', () => {
+    const newState = eventReducer(undefined, {})
+    expect(newState).toEqual({
+      eventsState: eventStates.LOADED,
+      data: []
+    })
+  })
+
+  it('Should return new state if receiving type', () => {
+    const events = [
+      { event: 'event1' },
+      { event: 'event2' },
+      { event: 'event3' }
+    ]
+    const newState = eventReducer(undefined, { type: GET_ALL_EVENTS_FINISH,
+      payload: events })
+    expect(newState).toEqual({
+      eventsState: eventStates.LOADED,
+      data: events
+    })
+  })
+})
